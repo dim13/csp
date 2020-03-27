ea76284082331a74b028174d22e0e04eaa369286
Dimitri Sokolyuk <demon@dim13.org>
2020-03-27 22:02:12 +0100

    add some tests

---
+++ csp_test.go
@@ -0,0 +1,23 @@
+package csp
+
+import "testing"
+
+func TestString(t *testing.T) {
+	var s []string
+	String{&s}.Set("a,b,c")
+	for n, v := range []string{"a", "b", "c"} {
+		if s[n] != v {
+			t.Error("got", s)
+		}
+	}
+}
+
+func TestInt(t *testing.T) {
+	var i []int
+	Int{&i}.Set("1,2,3")
+	for n, v := range []int{1, 2, 3} {
+		if i[n] != v {
+			t.Error("got", i)
+		}
+	}
+}
